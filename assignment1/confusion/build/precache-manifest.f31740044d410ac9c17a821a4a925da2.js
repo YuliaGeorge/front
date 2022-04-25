eee7508f3eb2f0c8374f365c2b5f8e34e806296c
YuliaGeorge <yulko361@gmail.com>
2022-04-25 21:11:11 +0300

    Rep for lab4

---
+++ assignment1/confusion/build/precache-manifest.f31740044d410ac9c17a821a4a925da2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e35d10a0e8859da22bcf8ea4c16047c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "47302b30be6579ee89bf",
+    "url": "/static/css/2.bb4ba17c.chunk.css"
+  },
+  {
+    "revision": "283ccec7ff90f83da991",
+    "url": "/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "47302b30be6579ee89bf",
+    "url": "/static/js/2.fcda6cde.chunk.js"
+  },
+  {
+    "revision": "283ccec7ff90f83da991",
+    "url": "/static/js/main.1b2c50f5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ assignment4/json-server/run.bat
@@ -0,0 +1,2 @@
+npx json-server --watch db.json -p 3001 -d 2000 
+
